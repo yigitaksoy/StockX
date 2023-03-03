557d56ae22db49ab3ebff3c07625b56a7c5235c7
yigitaksoy <yigitaksoy@gmail.com>
2023-03-03 19:35:21 +0100

    Add Overview component into dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,6 +2,7 @@ import Card from "./Card";
 import { mockCompanyDetails } from "../constants/Mock";
 import Header from "./Header";
 import Details from "./Details";
+import Overview from "./Overview";
 
 const Dashboard = () => {
   return (
@@ -13,7 +14,13 @@ const Dashboard = () => {
         <Card>Chart</Card>
       </div>
       <div>
-        <Card>Overview</Card>
+        <Overview
+          symbol={mockCompanyDetails.ticker}
+          price={300}
+          change={30}
+          changePercent={10.0}
+          currency={"USD"}
+        />
       </div>
       <div className="row-span-2 xl:row-span-3">
         <Details details={mockCompanyDetails} />
