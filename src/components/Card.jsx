e14387a64e0cec68455c28e1b58da988e9d9368e
yigitaksoy <yigitaksoy@gmail.com>
2023-03-03 18:18:31 +0100

    Add border and background styles for card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 const Card = ({ children }) => {
   return (
-    <div className="relative h-full w-full rounded-md border-2 p-8">
+    <div className="relative h-full w-full rounded-md border-2 border-neutral-200 bg-white p-8">
       {children}
     </div>
   );
