b9df8ab1e13429b56a48b1bbb8222630ab47fb9b
yigitaksoy <yigitaksoy@gmail.com>
2023-03-03 17:23:52 +0100

    Create Search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { mockSearchResults } from "../constants/Mock";
+import { XMarkIcon, MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+
+const Search = () => {
+  const [input, setInput] = useState("");
+  const [bestMatches, setBestMatches] = useState(mockSearchResults.result);
+
+  const clear = () => {
+    setInput("");
+    setBestMatches([]);
+  };
+
+  const updateBestMatches = () => {
+    setBestMatches(mockSearchResults.result);
+  };
+
+  return (
+    <div className="relative z-50 my-4 flex w-96 items-center rounded-md border-2 border-neutral-200 bg-white">
+      <input
+        type="text"
+        value={input}
+        className="w-full rounded-md px-4 py-2 focus:outline-none"
+        placeholder="Search"
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            updateBestMatches();
+          }
+        }}
+      />
+      {input && (
+        <button onClick={clear}>
+          <XMarkIcon className="h-4 w-4 fill-gray-500" />
+        </button>
+      )}
+
+      <button
+        onClick={updateBestMatches}
+        className="m-1 flex h-8 w-8 items-center justify-center rounded-md bg-neutral-900 p-2 transition hover:bg-neutral-700"
+      >
+        <MagnifyingGlassIcon className="h-4 w-4  fill-gray-100" />
+      </button>
+    </div>
+  );
+};
+
+export default Search;
