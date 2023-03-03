162d3a762111ebf0012e77646327f8509d2e11a5
yigitaksoy <yigitaksoy@gmail.com>
2023-03-03 17:24:30 +0100

    Add header component into dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,12 @@
 import Card from "./Card";
+import { mockCompanyDetails } from "../constants/Mock";
+import Header from "./Header";
 
 const Dashboard = () => {
   return (
-    <div className="xl:grid-rows-5 grid h-screen auto-rows-fr grid-cols-1 grid-rows-8 gap-6 p-10 font-sans md:grid-cols-2 md:grid-rows-7 xl:grid-cols-3">
-      <div className="col-span-1 row-span-1 md:col-span-2 xl:col-span-3">
-        <Card>Header</Card>
+    <div className="grid h-screen auto-rows-fr grid-cols-1 grid-rows-8 gap-6 p-10 font-sans md:grid-cols-2 md:grid-rows-7 xl:grid-cols-3 xl:grid-rows-5">
+      <div className="col-span-1 row-span-1 flex items-center justify-start md:col-span-2 xl:col-span-3">
+        <Header name={mockCompanyDetails.name} />
       </div>
       <div className="row-span-4 md:col-span-2">
         <Card>Chart</Card>
