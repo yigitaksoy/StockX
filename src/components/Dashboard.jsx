a3769a4a1ce1eb831df2393bd04dbb544fb04c65
yigitaksoy <yigitaksoy@gmail.com>
2023-03-06 19:52:11 +0100

    Fetch stock data from the API and pass it onto dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,29 +1,64 @@
-import { mockCompanyDetails } from "../constants/Mock";
-import Header from "./Header";
-import Details from "./Details";
+import React, { useContext, useEffect, useState } from "react";
+import fetchStockDetails from "../utilities/api/fetchStockDetails";
+import fetchQuote from "../utilities/api/fetchQuote";
+import StockContext from "../context/StockContext";
 import Overview from "./Overview";
+import Details from "./Details";
 import Chart from "./Chart";
+import Header from "./Header";
 
 const Dashboard = () => {
+  const { stockSymbol } = useContext(StockContext);
+  const [stockDetails, setStockDetails] = useState({});
+  const [quote, setQuote] = useState({});
+
+  useEffect(() => {
+    const updateStockDetails = async () => {
+      try {
+        const result = await fetchStockDetails(stockSymbol);
+        setStockDetails(result);
+      } catch (error) {
+        setStockDetails({});
+        console.log(error);
+      }
+    };
+
+    const updateStockOverview = async () => {
+      try {
+        const result = await fetchQuote(stockSymbol);
+        setQuote(result);
+      } catch (error) {
+        setQuote({});
+        console.log(error);
+      }
+    };
+
+    updateStockDetails();
+    updateStockOverview();
+  }, [stockSymbol]);
+
   return (
-    <div className="grid h-screen auto-rows-fr grid-cols-1 grid-rows-8 gap-6 bg-neutral-100 p-10 font-sans md:grid-cols-2 md:grid-rows-7 xl:grid-cols-3 xl:grid-rows-5">
+    <div
+      className="grid h-screen auto-rows-fr grid-cols-1 grid-rows-8 gap-6 p-10 font-sans md:grid-cols-2 md:grid-rows-7 xl:grid-cols-3 xl:grid-rows-5
+    "
+    >
       <div className="col-span-1 row-span-1 flex items-center justify-start md:col-span-2 xl:col-span-3">
-        <Header name={mockCompanyDetails.name} />
+        <Header name={stockDetails.name} />
       </div>
       <div className="row-span-4 md:col-span-2">
         <Chart />
       </div>
       <div>
         <Overview
-          symbol={mockCompanyDetails.ticker}
-          price={300}
-          change={30}
-          changePercent={10.0}
-          currency={"USD"}
+          symbol={stockSymbol}
+          price={quote.pc}
+          change={quote.d}
+          changePercent={quote.dp}
+          currency={stockDetails.currency}
         />
       </div>
       <div className="row-span-2 xl:row-span-3">
-        <Details details={mockCompanyDetails} />
+        <Details details={stockDetails} />
       </div>
     </div>
   );
