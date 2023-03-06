3ce61a141e57f872778593bd415ece6c17d2adfc
yigitaksoy <yigitaksoy@gmail.com>
2023-03-06 19:52:59 +0100

    Set stock from the search results

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,4 +1,7 @@
+import { useContext } from "react";
+import StockContext from "../context/StockContext";
 const SearchResults = ({ results }) => {
+  const { setStockSymbol } = useContext(StockContext);
   return (
     <ul className="custom-scrollbar absolute top-12 h-64 w-full overflow-y-scroll rounded-md border-2 border-neutral-200 bg-white">
       {results.map((item) => {
@@ -6,6 +9,9 @@ const SearchResults = ({ results }) => {
           <li
             key={item.symbol}
             className="m-2 flex cursor-pointer items-center justify-between rounded-md p-4 hover:bg-gray-200"
+            onClick={() => {
+              setStockSymbol(item.symbol);
+            }}
           >
             <span>{item.symbol}</span>
             <span>{item.description}</span>
