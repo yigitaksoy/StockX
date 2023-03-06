6c2549d0739f2a80a4f1183f5ebbd7105f66d6de
yigitaksoy <yigitaksoy@gmail.com>
2023-03-06 13:15:13 +0100

    Add date helpers

---
+++ src/helpers/dateHelper.js
@@ -0,0 +1,15 @@
+export const convertDateToUnixTimestamp = (date) => {
+  return Math.floor(date.getTime() / 100);
+};
+
+export const convertDateToUnixTimestampToDate = (unixTimeStamp) => {
+  const milliseconds = unixTimeStamp * 1000;
+  return new Date(milliseconds).toLocaleDateString();
+};
+
+export const createDate = (date, days, weeks, months, years) => {
+  let newDate = new Date(date);
+  newDate.setDate(newDate.getDate() + days + 7 * weeks);
+  newDate.setMonth(newDate.getMonth() + months);
+  newDate.setFullYear(newDate.getFullYear() + years);
+};
