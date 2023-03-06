26e31dd478e60b76f08670a50942e27b405b08ab
yigitaksoy <yigitaksoy@gmail.com>
2023-03-06 13:16:21 +0100

    Ad Chart component into Dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,17 +1,17 @@
-import Card from "./Card";
 import { mockCompanyDetails } from "../constants/Mock";
 import Header from "./Header";
 import Details from "./Details";
 import Overview from "./Overview";
+import Chart from "./Chart";
 
 const Dashboard = () => {
   return (
-    <div className="grid h-screen auto-rows-fr grid-cols-1 grid-rows-8 gap-6 p-10 font-sans md:grid-cols-2 md:grid-rows-7 xl:grid-cols-3 xl:grid-rows-5">
+    <div className="grid h-screen auto-rows-fr grid-cols-1 grid-rows-8 gap-6 bg-neutral-100 p-10 font-sans md:grid-cols-2 md:grid-rows-7 xl:grid-cols-3 xl:grid-rows-5">
       <div className="col-span-1 row-span-1 flex items-center justify-start md:col-span-2 xl:col-span-3">
         <Header name={mockCompanyDetails.name} />
       </div>
       <div className="row-span-4 md:col-span-2">
-        <Card>Chart</Card>
+        <Chart />
       </div>
       <div>
         <Overview
