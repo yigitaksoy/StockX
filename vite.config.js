ad8dd8c052854c82bdc03b9afc461726f0077f3e
yigitaksoy <yigitaksoy@gmail.com>
2023-03-06 13:17:52 +0100

    Fix undefined env variable issue for Vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  root: "src",
-  build: {
-    outDir: "../dist",
-  },
-});
+export default ({ mode }) => {
+  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
+
+  return defineConfig({
+    plugins: [react()],
+    root: "src",
+    build: {
+      outDir: "../dist",
+    },
+  });
+};
