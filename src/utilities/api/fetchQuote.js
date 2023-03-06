fda0da335d4729b022022e6f1cd455458bfebc51
yigitaksoy <yigitaksoy@gmail.com>
2023-03-06 19:35:10 +0100

    Add quote data API call

---
+++ src/utilities/api/fetchQuote.js
@@ -0,0 +1,16 @@
+import { finnhub } from "../../helpers/finnhubAPI";
+
+const fetchQuote = async (stockSymbol) => {
+  const apiKey = import.meta.env.VITE_FINNHUB_API_KEY;
+  const apiRes = await fetch(
+    `${finnhub}/quote?symbol=${stockSymbol}&token=${apiKey}`
+  );
+
+  if (!apiRes.ok) {
+    throw new Error(`Symbol ${stockSymbol} fetch not ok`);
+  }
+
+  return apiRes.json();
+};
+
+export default fetchQuote;
