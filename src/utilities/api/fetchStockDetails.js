b04afba8212ffd4a7a1fd819bbbfd91e541a7406
yigitaksoy <yigitaksoy@gmail.com>
2023-03-06 19:35:48 +0100

    Add stock details API call

---
+++ src/utilities/api/fetchStockDetails.js
@@ -0,0 +1,16 @@
+import { finnhub } from "../../helpers/finnhubAPI";
+
+const fetchStockDetails = async (stockSymbol) => {
+  const apiKey = import.meta.env.VITE_FINNHUB_API_KEY;
+  const apiRes = await fetch(
+    `${finnhub}/stock/profile2?symbol=${stockSymbol}&token=${apiKey}`
+  );
+
+  if (!apiRes.ok) {
+    throw new Error(`Symbol ${stockSymbol} fetch not ok`);
+  }
+
+  return apiRes.json();
+};
+
+export default fetchStockDetails;
