80fc6b82fff972404522398d77335fa6b41a10d9
yigitaksoy <yigitaksoy@gmail.com>
2023-08-04 13:18:06 +0200

    Adjust chart filter buttons for smaller screens

--- src/components/ChartFilters.jsx
+++ src/components/ChartFilters.jsx
@@ -2,7 +2,7 @@ const ChartFilters = ({ text, active, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className={`m-2 flex h-8 w-12 cursor-pointer items-center justify-center rounded-md border-1 ${
+      className={`m-2 flex h-6 w-6 cursor-pointer items-center justify-center rounded-md border-1 md:h-8 md:w-12 ${
         active
           ? "border-y-neutral-400 bg-black text-gray-100"
           : "border-black text-neutral-500"
