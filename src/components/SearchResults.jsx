64a3ea59ecb72a64bf5d306bbdd3f2abb519bb37
yigitaksoy <yigitaksoy@gmail.com>
2023-08-07 01:26:39 +0200

    Invoke onSelect prop when a search result is clicked

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,7 +1,14 @@
 import { useContext } from "react";
 import StockContext from "../context/StockContext";
-const SearchResults = ({ results }) => {
+
+const SearchResults = ({ results, onSelect }) => {
   const { setStockSymbol } = useContext(StockContext);
+
+  const handleItemClick = (item) => {
+    setStockSymbol(item.symbol);
+    onSelect(item);
+  };
+
   return (
     <ul className="custom-scrollbar absolute top-12 h-64 w-full overflow-y-scroll rounded-md border-2 border-neutral-200 bg-white">
       {results.map((item) => {
@@ -9,9 +16,7 @@ const SearchResults = ({ results }) => {
           <li
             key={item.symbol}
             className="m-2 flex cursor-pointer items-center justify-between rounded-md p-4 hover:bg-gray-200"
-            onClick={() => {
-              setStockSymbol(item.symbol);
-            }}
+            onClick={() => handleItemClick(item)}
           >
             <span>{item.symbol}</span>
             <span>{item.description}</span>
