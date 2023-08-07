7a4850c539f93c6a9451d9f10737a5b45383081a
yigitaksoy <yigitaksoy@gmail.com>
2023-08-07 02:54:21 +0200

    Reduce card padding

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({ children }) => {
   const { darkMode } = useContext(ThemeContext);
   return (
     <div
-      className={`relative h-full w-full rounded-md border-2 p-8 transition duration-700 ${
+      className={`relative h-full w-full rounded-md border-2 p-6 transition duration-700 ${
         darkMode
           ? "border-neutral-700 bg-zinc-900"
           : "bg-white text-neutral-400"
