11910ee157c7a3ce54f98489880a9f99c8ed85eb
yigitaksoy <yigitaksoy@gmail.com>
2023-08-07 04:15:52 +0200

    Add default symbols for stock ticker

--- src/components/StockTicker.jsx
+++ src/components/StockTicker.jsx
@@ -4,6 +4,36 @@ import { TickerTape } from "react-ts-tradingview-widgets";
 
 const StockTicker = () => {
   const { darkMode } = useContext(ThemeContext);
+  const symbols = [
+    {
+      proName: "FOREXCOM:SPXUSD",
+      title: "S&P 500",
+    },
+    {
+      proName: "FOREXCOM:NSXUSD",
+      title: "Nasdaq 100",
+    },
+    {
+      proName: "NYSE:PVH",
+      title: "PVH/USD",
+    },
+    {
+      proName: "NASDAQ:ABNB",
+      title: "Airbnb",
+    },
+    {
+      proName: "NASDAQ:AMZN",
+      title: "Amazon",
+    },
+    {
+      proName: "FX_IDC:EURUSD",
+      title: "EUR/USD",
+    },
+    {
+      proName: "FX_IDC:EURGBP",
+      title: "EUR/GBP",
+    },
+  ];
   const styles = {
     parent: {
       fontSize: ".01px",
@@ -21,6 +51,7 @@ const StockTicker = () => {
     >
       <TickerTape
         colorTheme={`${darkMode ? "dark" : "light"}`}
+        symbols={symbols}
         copyrightStyles={styles}
       ></TickerTape>
     </div>
